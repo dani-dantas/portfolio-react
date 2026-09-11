b28ec68992205d8e417c7622c448242d1304bb4b
Daniella Dantas <soudanielladantas@gmail.com>
2026-09-11 17:01:36 -0300

    alterações em sobre mim

--- src/pages/AboutMePage/index.jsx
+++ src/pages/AboutMePage/index.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => (
         <div id="sobreMimTextosFlexContainer">
             <div id="sobreMimTextosFlexItem">
                 <h1 className="titulo">Sobre mim</h1>
-                <p className="lineHeight">Olá! Sou <strong>Daniella Dantas</strong>, estudante de Engenharia de Software e formada em ADS. Comecei minha trajetória no Front-end, com HTML, CSS, JavaScript e React, e atualmente estou aprofundando meus conhecimentos em Back-end Java, Spring Boot, APIs REST e bancos de dados. Gosto de entender como as coisas funcionam, identificar problemas e transformar ideias em soluções práticas, aprendendo na prática por meio de projetos e desafios. Meu objetivo é construir uma base sólida em desenvolvimento de software e evoluir para atuar como desenvolvedora Full Stack.</p>
+                <p className="lineHeight">Olá! Sou <strong>Daniella Dantas</strong>, estudante de Engenharia de Software e formada em ADS. Comecei minha trajetória no Front-end, com <strong>HTML, CSS, JavaScript e React</strong>, e atualmente estou aprofundando meus conhecimentos em Back-end <strong>Java, Spring Boot, APIs REST e bancos de dados</strong>. Gosto de entender como as coisas funcionam, identificar problemas e transformar ideias em soluções práticas, aprendendo na prática por meio de projetos e desafios. Meu objetivo é construir uma base sólida em desenvolvimento de software e evoluir para atuar como desenvolvedora Full Stack.</p>
                 <Button buttonType="linkedin" />
             </div>
         </div>
